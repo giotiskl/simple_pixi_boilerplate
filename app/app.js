e3119e0fb0a595fab86fb51e684813c51bd521bb
Yiotis Kaltsikis <giotisgr@gmail.com>
2016-11-18 14:33:49 +0100

    structure project with different folders for app and styles

---
+++ app/app.js
@@ -0,0 +1,17 @@
+// get canvas and context
+const canvas = document.getElementById('app'),
+      ctx = canvas.getContext('2d');
+
+ctx.fillStyle = 'lightgray';
+ctx.fillRect(0,0,640,480);
+
+// draw text outline
+ctx.fillStyle = 'black';
+ctx.font = '36px sans-serif';
+ctx.textAlign = 'center';
+ctx.lineWidth = 4;
+ctx.strokeText('This is a sample canvas', 640 / 2, 480 / 2);
+
+// fill text
+ctx.fillStyle = 'red';
+ctx.fillText('This is a sample canvas', 640 / 2, 480 / 2);
